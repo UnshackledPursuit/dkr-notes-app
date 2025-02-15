1820c01e6bc696981c45ecfd3adfc256377c38eb
Dylan Russell <UnshackledPursuit@gmail.com>
2025-02-14 20:30:01 -0500

    feat: add drizzle, clerk, and stripe dependencies with initial db setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,15 @@
-import { config } from "dotenv";
-import { defineConfig } from "drizzle-kit";
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
 
-config({ path: ".env.local" });
-
-export default defineConfig({
+export default {
   schema: "./db/schema/index.ts",
-  out: "./db/migrations",
+  out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!
-  }
-});
+    host: process.env.DATABASE_URL?.split('@')[1]?.split(':')[0] ?? '',
+    user: process.env.DATABASE_URL?.split('://')[1]?.split(':')[0] ?? '',
+    password: process.env.DATABASE_URL?.split(':')[2]?.split('@')[0] ?? '',
+    database: 'postgres',
+  },
+} satisfies Config;
